59e42a5db054077234e08b8f77de5909b45f1b23
Cristian-Quiza <cristian.david_95@hotmail.com>
2022-09-02 20:57:31 -0500

    Plantilla PetCat Frontend

---
+++ HolaWeb.App.Frontend/Pages/Services.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HolaWeb.App.Frontend.Pages;
+
+public class ServicesModel : PageModel
+{
+    private readonly ILogger<ServicesModel> _logger;
+
+    public ServicesModel(ILogger<ServicesModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
